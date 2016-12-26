6d03a4bdef4252f91aaf7fef846d19d8642e7c7f
Octávio Richter <octavio.richter@neoway.com.br>
2016-12-26 19:50:27 -0200

    Moves user struct

--- api/main.go
+++ api/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
     "net/http"
     "encoding/json"
+    "github.com/NeowayLabs/dojo/api/user"
 )
 
 func check(e error) {
@@ -13,16 +14,11 @@ func check(e error) {
 	}
 }
 
-type User struct {
-	Name     string
-	Password string
-}
-
 func newUserHandler(rw http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(req.Body)
     defer req.Body.Close()
 
-    var u User
+    var u user.User
     err := decoder.Decode(&u)
 	check(err)
 
@@ -30,8 +26,10 @@ func newUserHandler(rw http.ResponseWriter, req *http.Request) {
     f, err := os.OpenFile("db.json", os.O_APPEND|os.O_WRONLY, 0600)
     check(err)
     defer f.Close()
+
     ju, err := json.Marshal(u)
     check(err)
+
     _, err = f.Write(ju)
     check(err)
 }
