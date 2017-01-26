a617b8063d3d768aa6e5908b31c18b30b62cdfa8
Octávio Richter <octavio.richter@neoway.com.br>
2017-01-26 20:35:56 -0200

    Tests new user validation

--- api/model/user_test.go
+++ api/model/user_test.go
@@ -9,17 +9,37 @@ import (
 func TestNewUser(t *testing.T) {
 	clearDB() // Warning! Before testing we wipe out the DB!
 
-	data := []byte(`{"name": "octavio", "password":"588a56af5df74a9f36b09575"}`)
-	req := httptest.NewRequest("POST", "/user/new", bytes.NewReader(data))
-	rw := httptest.NewRecorder()
+	t.Run("valid user in empty db", func(t *testing.T) {
+		data := []byte(`{"name": "octavio", "password":"588a56af5df74a9f36b09575"}`)
+		req := httptest.NewRequest("POST", "/user/new", bytes.NewReader(data))
+		rw := httptest.NewRecorder()
 
-	UserNewHandler(rw, req)
+		UserNewHandler(rw, req)
 
-	if rw.Code != 200 {
-		t.Error("Wrong response code")
-	}
+		if rw.Code != 200 {
+			t.Error("Wrong response code")
+		}
 
-	if rw.Body.String() != "Saved user octavio" {
-		t.Error("Wrong response body")
-	}
+		if rw.Body.String() != "Saved user octavio" {
+			t.Error("Wrong response body")
+		}
+	})
+
+	t.Run("alredy existing user", func(t *testing.T) {
+		data := []byte(`{"name": "octavio", "password":"588a56af5df74a9f36b09575"}`)
+		req := httptest.NewRequest("POST", "/user/new", bytes.NewReader(data))
+		rw := httptest.NewRecorder()
+
+		UserNewHandler(rw, req)
+
+		if rw.Code != 500 {
+			t.Error("Wrong response code")
+		}
+
+		if rw.Body.String() != "User already exists\n" {
+			t.Error("Wrong response body")
+		}
+	})
+
+	clearDB()
 }
